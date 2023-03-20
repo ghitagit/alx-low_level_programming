7796445753e4bc0b3041886283ca19405ba4ea27
Ghitagitt <boueddahar.ghita02@gmail.com>
2023-03-20 16:46:36 -0700

    hopethisone

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,41 +1,30 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
+ * main - Prints all possible combinations of two different digits,
+ *        in ascending order, separated by a comma followed by a space.
  *
- * Return: Always 0 (Success/completed)
+ * Return: Always 0.
  */
 int main(void)
-
 {
-int number_left;
-int number_right;
+	int digit1, digit2;
 
+	for (digit1 = 0; digit1 < 9; digit1++)
+	{
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+		{
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
 
-for (number_left = 48; number_right <= 78; number_left++)
-{
-for (number_right = number_left + 1 ; number_right <= 78; number_right++)
-{
+			if (digit1 == 8 && digit2 == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
 
-putchar(number_left);
-putchar (number_right);
+	putchar('\n');
 
-
-if ((number_left == 56) && (number_right == 78))
-{
-break;
-}
-
-putchar(',');
-putchar (' ');
-
-}
-
-}
-
-putchar('\n');
-
-
-return (0);
-
-
+	return (0);
 }
--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,25 +1,22 @@
-#include <stdio.h>
+#include <stdio.h>
+
 /**
- * main - entry point
+ * main - Prints all the numbers of base 16 in lowercase.
  *
- * Return: Always 0 (success/correct)
+ * Return: Always 0.
  */
 int main(void)
-
 {
-char base_16;
+	int num;
+	char letter;
 
-for (base_16 = 48; base_16 <= 57; base_16++)
-{putchar(base_16);
-}
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
 
-for (base_16 = 97 ; base_16 <= 102; base_16++)
-{
-putchar(base_16);
-}
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchar(letter);
 
-putchar ('\n');
+	putchar('\n');
 
-return (0);
-
+	return (0);
 }
